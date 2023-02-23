8918ed997ce20c47060278ecbb1039096224610a
Shreeniwas Chaudhari <86178940+shree-10@users.noreply.github.com>
2023-02-23 16:53:32 +0530

    Add files via upload

---
+++ shreeniwas/testapp/urls.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from django.urls import path, include
+from .views import *
+from .import views
+
+urlpatterns = [
+    path('', views.retrive_view, name="home"),
+    path('create/', views.create_view,name="crete_view"),
+    path('delete/<int:id>',views.delete_view,name="delete view"),
+    path('update/<int:id>', views.update_view,name="update_view"),
+    
+]
